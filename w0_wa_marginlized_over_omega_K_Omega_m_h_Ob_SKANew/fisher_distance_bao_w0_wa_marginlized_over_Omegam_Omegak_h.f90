91d7eb7d80605443aee649f03733855ba3bae187
sahba <sahbayahya@gmail.com>
2014-07-14 21:47:33 +0200

    organized

--- w0_wa_marginlized_over_omega_K_Omega_m_h_Ob_SKANew/fisher_distance_bao_w0_wa_marginlized_over_Omegam_Omegak_h.f90
+++ w0_wa_marginlized_over_omega_K_Omega_m_h_Ob_SKANew/fisher_distance_bao_w0_wa_marginlized_over_Omegam_Omegak_h.f90
@@ -60,15 +60,15 @@ PROGRAM Fisher_Distance
   CALL setup_da       ! tabulate the angular diameter distance
   ! ask for survey specific parameters
 !==============Enter the File name ================================== 
-filename = 'number_sax3_7point3_mJy_SKANew_S3.txt'
+!filename = 'number_sax3_7point3_mJy_SKANew_S3.txt'
 !filename='number_7point3_z=1.txt'!
-!filename= 'number_EuclidmJy_ref_z=1.txt'
+filename= 'number_EuclidmJy_ref.txt'
   open(2,file=filename,status='unknown')
   
 !=============  Read in linear P(k) ===================================
   filename='linear_matterpower_1.dat' 
 ! filename= 'wmap5baosn_max_likelihood_matterpower_at_z=30.dat'
-  n=869 ! no. of lines in the file
+  n=127 ! no. of lines in the file
   zin=1d0 ! redshift of the input power spectrum
   CALL open_linearpk(filename,n) ! This fuction just to interpolate the values in the filename
 !================================================================  
@@ -79,9 +79,6 @@ filename = 'number_sax3_7point3_mJy_SKANew_S3.txt'
   Vtot=0d0   ! total survey volume
   nbins=0    ! initialize the number of bins
   !=========== Loop over redshift bins =======================================
-  
-
-  
   do ibin=1,100
      read(2,*,end=10)area,zmin,zmax,dndz,bias,kmax_ov_h,kmin_ov_h,delta_v ! uncomment for Euclid
     ! read(2,*,end=10)area,zmin,zmax,dndz,bias,kmax_ov_h,delta_v ! Uncomment for SKA
@@ -93,18 +90,17 @@ filename = 'number_sax3_7point3_mJy_SKANew_S3.txt'
      ngal=dndz !  Uncomment for  Euclid
     !ngal=dndz/((3.14159d0/180d0)**2*dvdz(z)) ! Uncomment for SKA
      Vtot=Vtot+Vsurvey
-     print*, 'da', da(z)
+    ! print*, 'da', da(z)
    ! Vsurvey =  13.38d0*10d0**9d0/(H_0/100d0)**3d0 ! Phil 
      fz =(1d0 + z)**(3d0*(1d0 + w0+ w_a)) * exp(-3d0 * w_a *(z/(1d0+ z)))
      sigma_z= (1d0+z)*(delta_v/2.998d5) &
           *((c/H_0)/dsqrt((om0+ob0)*(1d0+z)**3d0+ok0* (1d0+z)**2d0+ ode0* fz)) ! in units of h^-1 Mpc
      sigma_perp=sigma0*g(z)/(1d0+z) ! in units of h^-1 Mpc
      sigma_para=sigma0*g(z)/(1d0+z)*(2d0+dgdlna(z)/g(z)) ! in units of h^-1 Mpc
+     !==================Test the growth =================================
     !    open(101,file='nz_fz_Dz_test_rms=73.txt' ,status='unknown')
    !	write(101,*) z,ngal, (1d0+dgdlna(z)/g(z)), g(z)/(1d0+z)
-     !
    !=================  computing the Fisher matrix... =================================
-     
      open(11,file=filename,status='old')
      read(11,*)k0_ov_h,dummy
      k_ov_h=k0_ov_h
@@ -113,12 +109,12 @@ filename = 'number_sax3_7point3_mJy_SKANew_S3.txt'
      !================ loop over k ===================================
      do while (k_ov_h<=kmax_ov_h)
         read(11,*)k_ov_h,dummy
-
-        !dlnk=dlog(k_ov_h)-dlog(k0_ov_h)
-          k0_new=6.8492999999999996E-005 
+        !dlnk=dlog(k_ov_h)-dlog(k0_ov_h) ! uncomment
+        !print*, 'dlnk', dlnk 
+        !===========================================================
+         k0_new=6.8492999999999996E-005 
          k_new =9.5589999999999998E-005
-         dlnk=dlog(k_new)-dlog(k0_new) !uncomment to use with Plank's parameters 
-        !print*, 'dlnk', dlnk
+         dlnk=dlog(k_new)-dlog(k0_new)                         !uncomment to use with Plank's parameters 
         factor=(k_ov_h)**3d0*dlnk/(2.d0 *2d0 * 2d0 *3.1415926535d0**2d0) ! h^3 Mpc^-3
         !=========== loop over mu..========================
          mu=0d0
@@ -145,7 +141,7 @@ filename = 'number_sax3_7point3_mJy_SKANew_S3.txt'
             wdamp_para =exp(-(k_ov_h*sigma_para)**2d0*mu**2d0)
             wdamp_silk = exp(-2d0*(k_ov_h*sigma_silk)**1.4d0)
             wdamp_factor = dsqrt(2d0 *3.1415926535d0**2d0) * BAO_AMP *  p02
-            wdamp_sinterm = ( dsin(k_ov_h *150d0)/(k_ov_h*150d0))
+            wdamp_sinterm = ( dsin(k_ov_h *100d0)/(k_ov_h*150d0))
            Pb= wdamp_factor *  wdamp_sinterm * wdamp_perp  &
            *  wdamp_para *  wdamp_silk
            der(1)=(1d0-mu2) ! dlnPkdlnDa
@@ -157,19 +153,21 @@ filename = 'number_sax3_7point3_mJy_SKANew_S3.txt'
            enddo
            mu=mu+dmu
         enddo
+       !================= save files ====================================== 
       ! open(5,file='Pk_Euclid_testPlank.txt' ,status='unknown')
-   !    write(5,*) k_ov_h, pk! , 1d0/ngal
+     ! write(5,*) k_ov_h, pk! , 1d0/ngal
      !  open(5,file='gz.txt' ,status='unknown')
      !  write(5,*)  z, g(z)
     !	open(100,file='cosmic_v_k_Euclid_fis_test.txt' ,status='unknown')
     !	write(100,*) k_ov_h,  dsqrt(1d0/fistest(1,1))!
     ! 	write(5,*) ((1d0/P02)*dvdz(z) ), (dndz*((3.14159d0/180d0)**2*dvdz(z)))
     ! 	print*, 1d0/P02, dvdz(z)
-    ! 	write(5,*) z, ((1d0/P02)*dvdz(z) ), (dndz*((3.14159d0/180d0)**2*dvdz(z)))
-    open(5,file='Pb_vs_k_testz=1.txt' ,status='unknown')
-    write(5,*) k_ov_h , Pb
-   ! open(150,file='wdamps_testz=1.txt' ,status='unknown')
-   ! write(150,*) k_ov_h , wdamp_perp, wdamp_para, wdamp_silk, wdamp_factor, wdamp_sinterm
+   ! 	write(5,*) z, ((1d0/P02)*dvdz(z) ), (dndz*((3.14159d0/180d0)**2*dvdz(z)))
+   !  open(5,file='Pb_vs_k_testz=1.txt' ,status='unknown')
+   !   write(5,*) k_ov_h , Pb
+   !   open(150,file='wdamps_testz=1.txt' ,status='unknown')
+   !  write(150,*) k_ov_h , wdamp_perp, wdamp_para, wdamp_silk, wdamp_factor, wdamp_sinterm
+   !======================================================================
         k0_ov_h=k_ov_h
       	enddo
       	fistot=fistot+fis
@@ -231,10 +229,12 @@ SUBROUTINE report_result(z,bias,npara,fis)
   err_lnh=sqrt(cov(2,2))
   err_lnR=err_lnda*sqrt((1d0-r12**2d0) &
        /(1d0+2d0*r12*err_lnda/err_lnh+(err_lnda/err_lnh)**2d0))
-open(12,file='Fisher_7point3mJy_diff_14bins_S3.txt' ,status='unknown')
-  write(12,'(4F18.5)') err_lnda,err_lnh,err_lnR
- open(13,file='output_7point3mJy_diff_14bins_S3.txt', status='unknown')
- write(13,'(6F18.5)') z, err_lnda*1d2,err_lnh*1d2,err_lnR*1d2, beta
+ !==============save desired fisher files =======================
+!open(12,file='Fisher_7point3mJy_diff_14bins_S3.txt' ,status='unknown')
+!  write(12,'(4F18.5)') err_lnda,err_lnh,err_lnR
+! open(13,file='output_7point3mJy_diff_14bins_S3.txt', status='unknown')
+! write(13,'(6F18.5)') z, err_lnda*1d2,err_lnh*1d2,err_lnR*1d2, beta
+!========================================================
   print'(1A15,1F9.5)','Err[lnDa](%) =',err_lnda*1d2
   print'(1A15,1F9.5)','Err[lnH](%)  =',err_lnh*1d2
   print'(1A15,1F9.5)','r(lnDa,lnH)  =',r12
@@ -244,21 +244,15 @@ open(12,file='Fisher_7point3mJy_diff_14bins_S3.txt' ,status='unknown')
 END SUBROUTINE report_result
 
 !===========================================================
-
-
 SUBROUTINE report_result3x3(fis)
   USE cosmo
   IMPLICIT none
-  
   double precision, intent(IN) :: fis(6,6)
   double precision :: work(6),cov(6,6), A(6,6), M55DET, DET5x5,DET2x2,A05(6,6)
   integer :: i, j 
-  !print*,'*** Error on w***'
- ! print'(1A20,1F9.5)','Err[w]            =',1d0/dsqrt(fis(1,1))
   cov=fis
  !==============================Calculate DET 2x2 ===============
   CALL DVERT(cov,6,6,work)
- ! A = fis
   A = fis
    DET2x2 =  A(1,1)*A(2,2) - A(1,2)*A(2,1)  
   write(12,*)'*** Error on w, with  Om0, ob0, ok0 and  H0 marginalized over ***'
@@ -270,7 +264,6 @@ SUBROUTINE report_result3x3(fis)
    , A(6,1),  A(6,2),  A(6,3), A(6,4),  A(6,5),  A(6,6), ']'     
    
  !====================write the resutls to file 12 ========================
-   
    write(12,*),'Figure of Merit  =' ,  1d0/dsqrt((cov(1,1) * cov(2,2) - cov(1,2)* cov(2,1)))    
   write(12,'(1A20,1F9.5)')'Err[w] =',dsqrt(cov(1,1))
   write(12,'(1A20,1F9.5)')'Err[wa] =',dsqrt(cov(2,2))
@@ -286,9 +279,7 @@ SUBROUTINE report_result3x3(fis)
  write(12,'(1A20,1F9.5)') 'Err[H_0] =',dsqrt(cov(4,4))
   return
 END SUBROUTINE report_result3x3
-
 !================================================================
-
 SUBROUTINE transform_fisher(z,fisDH,fis3x3)
   USE cosmo
   IMPLICIT none
@@ -296,7 +287,6 @@ SUBROUTINE transform_fisher(z,fisDH,fis3x3)
   double precision, intent(IN)    :: z,fisDH(2,2)
   double precision, intent(INOUT) :: fis3x3(6,6)
   double precision :: dpdq(6,2)
- ! double precision :: ob_accuracy = 0.0008554077135804337, h_accuracy= 1.4d0,  om_accuracy=0.02d0 
   double precision :: chi,h2,func0,func1,func2,func3,func4,rombint,fz
   external h2,func0,func1,func2,func3,func4,rombint
   chi=rombint(func0,0d0,z,1d-7)
@@ -325,9 +315,7 @@ SUBROUTINE transform_fisher(z,fisDH,fis3x3)
   enddo
   return
 END SUBROUTINE transform_fisher
-
 !===============================================================
-
 SUBROUTINE add_cmb(fis3x3)
   USE cosmo
   ! Add the distance information from CMB [Eq.(38) of Shoji et al. 2008]
@@ -361,7 +349,7 @@ SUBROUTINE add_cmb(fis3x3)
   ! add Omega_matter
  ! fis3x3(1,1) = fis3x3(1,1) + .172276d6
  ! fis3x3(2,2)= fis3x3(2,2) + .139551d5
-  !fis3x3(3,3) = fis3x3(3,3) + 1d0/(0.115717676721)**2d0 !.253489d8 
+ !fis3x3(3,3) = fis3x3(3,3) + 1d0/(0.115717676721)**2d0 !.253489d8 
  !fis3x3(4,4)=fis3x3(4,4)+  1d0/(h_accuracy)**2d0
  ! fis3x3(5,5)=fis3x3(5,5)+   1d0/(om_accuracy)**2d0
  ! fis3x3(6,6)=fis3x3(6,6)+   1d0/(ob_accuracy)**2d0 
@@ -373,7 +361,6 @@ SUBROUTINE add_cmb(fis3x3)
   print*, '============================================================='
   return
 END SUBROUTINE add_cmb
-
 !========================================================
 !==============! Functions ==================================
 DOUBLE PRECISION FUNCTION h2(redshift)
@@ -429,7 +416,6 @@ DOUBLE PRECISION FUNCTION func3(redshift)
   func3 =( (1d0+redshift)**3d0 - fz) /h2(redshift)**1.5d0
   return
 END FUNCTION func3
-
 DOUBLE PRECISION FUNCTION func4(redshift)
   USE cosmo
   ! func4(z) = ln(1+z) - z/1+z /[h2(z)]^1.5
